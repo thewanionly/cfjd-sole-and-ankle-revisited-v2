c7cfe457fe28eda460e105c3ff62ecf57356e664
thewanionly <pelloani@gmail.com>
2025-03-12 04:03:17 +0800

    exercise-1: set up breakpoints

--- src/constants.js
+++ src/constants.js
@@ -5,14 +5,25 @@ export const COLORS = {
     300: 'hsl(190deg 5% 80%)',
     500: 'hsl(196deg 4% 60%)',
     700: 'hsl(220deg 5% 40%)',
-    900: 'hsl(220deg 3% 20%)',
+    900: 'hsl(220deg 3% 20%)'
   },
   primary: 'hsl(340deg 65% 47%)',
-  secondary: 'hsl(240deg 60% 63%)',
+  secondary: 'hsl(240deg 60% 63%)'
 };
 
 export const WEIGHTS = {
   normal: 500,
   medium: 600,
-  bold: 800,
+  bold: 800
+};
+
+// values are expressed in rems
+export const BREAKPOINTS = {
+  tabletMax: 59.375, // 950px
+  mobileMax: 37.5 // 600px;
+};
+
+export const SCREEN_MEDIA_QUERIES = {
+  tableAndBelow: `(max-width: ${BREAKPOINTS.tabletMax}rem)`,
+  mobileAndBelow: `(max-width: ${BREAKPOINTS.mobileMax}rem)`
 };
