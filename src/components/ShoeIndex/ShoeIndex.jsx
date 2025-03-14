7a8ab4b72268aafd2ad7e62e242212d6a2736b00
thewanionly <pelloani@gmail.com>
2025-03-15 04:22:23 +0800

    exercise-3: hide leftcolumn and reposition breadcrumbs at tablet and smaller

--- src/components/ShoeIndex/ShoeIndex.jsx
+++ src/components/ShoeIndex/ShoeIndex.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { WEIGHTS } from '../../constants';
+import { WEIGHTS, SCREEN_MEDIA_QUERIES } from '../../constants';
 
 import Breadcrumbs from '../Breadcrumbs';
 import Select from '../Select';
@@ -9,32 +9,35 @@ import Spacer from '../Spacer';
 import ShoeSidebar from '../ShoeSidebar';
 import ShoeGrid from '../ShoeGrid';
 
+const ShoeBreadcrumbs = ({ className }) => {
+  return (
+    <Breadcrumbs className={className}>
+      <Breadcrumbs.Crumb href='/'>Home</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href='/sale'>Sale</Breadcrumbs.Crumb>
+      <Breadcrumbs.Crumb href='/sale/shoes'>Shoes</Breadcrumbs.Crumb>
+    </Breadcrumbs>
+  );
+};
+
 const ShoeIndex = ({ sortId, setSortId }) => {
   return (
     <Wrapper>
       <MainColumn>
         <Header>
-          <Title>Running</Title>
-          <Select
-            label="Sort"
-            value={sortId}
-            onChange={(ev) => setSortId(ev.target.value)}
-          >
-            <option value="newest">Newest Releases</option>
-            <option value="price">Price</option>
+          <RightHeader>
+            <MobileShoeBreadcrumbs />
+            <Title>Running</Title>
+          </RightHeader>
+          <Select label='Sort' value={sortId} onChange={(ev) => setSortId(ev.target.value)}>
+            <option value='newest'>Newest Releases</option>
+            <option value='price'>Price</option>
           </Select>
         </Header>
         <Spacer size={32} />
         <ShoeGrid />
       </MainColumn>
       <LeftColumn>
-        <Breadcrumbs>
-          <Breadcrumbs.Crumb href="/">Home</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale">Sale</Breadcrumbs.Crumb>
-          <Breadcrumbs.Crumb href="/sale/shoes">
-            Shoes
-          </Breadcrumbs.Crumb>
-        </Breadcrumbs>
+        <DesktopShoeBreadcrumbs />
         <Spacer size={42} />
         <ShoeSidebar />
       </LeftColumn>
@@ -51,6 +54,24 @@ const Wrapper = styled.div`
 
 const LeftColumn = styled.div`
   flex-basis: 248px;
+
+  @media ${SCREEN_MEDIA_QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+
+const DesktopShoeBreadcrumbs = styled(ShoeBreadcrumbs)`
+  @media ${SCREEN_MEDIA_QUERIES.tabletAndSmaller} {
+    display: none;
+  }
+`;
+
+const MobileShoeBreadcrumbs = styled(ShoeBreadcrumbs)`
+  display: none;
+
+  @media ${SCREEN_MEDIA_QUERIES.tabletAndSmaller} {
+    display: flex;
+  }
 `;
 
 const MainColumn = styled.div`
@@ -61,6 +82,12 @@ const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: baseline;
+`;
+
+const RightHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
 `;
 
 const Title = styled.h2`
