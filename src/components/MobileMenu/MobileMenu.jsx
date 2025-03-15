8b587e877e58041ed05c56c532b60c1f17812716
thewanionly <pelloani@gmail.com>
2025-03-15 19:36:04 +0800

    exercise-4: show and style mobile menu

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -4,30 +4,101 @@ import styled from 'styled-components';
 import UnstyledButton from '../UnstyledButton';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
+import * as Dialog from '@radix-ui/react-dialog';
+
+import { SCREEN_MEDIA_QUERIES, COLORS, WEIGHTS } from '../../constants';
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
-
   return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
+    <Dialog.Root open={isOpen} onOpenChange={onDismiss}>
+      <Dialog.Portal>
+        <Overlay />
+
+        <Content>
+          <CloseButton onClick={onDismiss}>
+            <Icon id='close' />
+            <VisuallyHidden>Dismiss menu</VisuallyHidden>
+          </CloseButton>
+
+          <VisuallyHidden>
+            <Dialog.Title>Mobile navigation</Dialog.Title>
+            <Dialog.Description>Mobile navigation</Dialog.Description>
+          </VisuallyHidden>
+
+          <MobileNav>
+            <MobileNavLink href='/sale'>Sale</MobileNavLink>
+            <MobileNavLink href='/new'>New&nbsp;Releases</MobileNavLink>
+            <MobileNavLink href='/men'>Men</MobileNavLink>
+            <MobileNavLink href='/women'>Women</MobileNavLink>
+            <MobileNavLink href='/kids'>Kids</MobileNavLink>
+            <MobileNavLink href='/collections'>Collections</MobileNavLink>
+          </MobileNav>
+
+          <Footer>
+            <FooterLink href='/terms'>Terms and Conditions</FooterLink>
+            <FooterLink href='/privacy'>Privacy Policy</FooterLink>
+            <FooterLink href='/contact'>Contact Us</FooterLink>
+          </Footer>
+        </Content>
+      </Dialog.Portal>
+    </Dialog.Root>
   );
 };
+
+const Overlay = styled(Dialog.Overlay)`
+  position: fixed;
+  inset: 0;
+  background-color: hsl(220deg 5% 40% / 0.8);
+`;
+
+const Content = styled(Dialog.Content)`
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: ${COLORS.white};
+
+  width: max(min(300px, 80vw), 200px);
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 32px;
+`;
+
+const CloseButton = styled(UnstyledButton)`
+  align-self: flex-end;
+  margin-top: -6px;
+  margin-right: -16px;
+`;
+
+const MobileNav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+`;
+
+const MobileNavLink = styled.a`
+  font-size: 1.125rem;
+  font-weight: ${WEIGHTS.medium};
+  text-transform: uppercase;
+  text-decoration: none;
+  color: ${COLORS.gray[900]};
+
+  &:first-of-type {
+    color: ${COLORS.secondary};
+  }
+`;
+
+const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const FooterLink = styled.a`
+  font-size: 14px;
+  text-decoration: none;
+  color: ${COLORS.gray[700]};
+`;
 
 export default MobileMenu;
